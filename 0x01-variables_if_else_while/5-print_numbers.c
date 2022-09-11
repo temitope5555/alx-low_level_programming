841b715652f4fcf504ebb608d1b45db0250e4a04
temitope5555 <wwwtope947@gmail.com>
2022-09-11 15:38:58 -0700

    stubborn 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
+
 /**
-  * main - Prints the numbers since 0 to 9
-  *
-  * Return: Always (Success)
-  */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-        char n;
-        for (n = '0'; n <= '9'; n++)
-        {
-                putchar(n);
-        }
-        putchar('\n');
-        return (0);
+	int d = 48;
+
+	while (d < 58)
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+
+	return (0);
 }
