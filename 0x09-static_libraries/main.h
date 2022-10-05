1907f333675897d471c77223e47922632bc89ca6
temitope5555 <wwwtope947@gmail.com>
2022-10-05 11:17:47 -0700

    werey code

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,12 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
- *  * File: main.h
- *   * Auth: Alagwu David
- *    * Desc: Header file containing the prototypes of all functions
- *     *       included in the static library libholberton.a.
- *      */
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -28,4 +23,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* MAIN_H */
