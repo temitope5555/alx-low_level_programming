caa8795a7bf7ad2a545bc413c4127c1ffe1952fa
temitope5555 <wwwtope947@gmail.com>
2022-10-11 01:00:15 -0700

    pls

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,13 +1,10 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
+#ifndef OBJECT_LIKE_MACRO_H
 
-/*
- * File: 3-function_like_macro.h
- * Auth: Ismail Mohamed Ismail
- * Desc: Header file that defines a function-like macro ABS(x)
- *       that computes the absolute value of a number x.
- */
+#define OBJECT_LIKE_MACRO_H
 
-#define ABS(x) ((x) < 0 ? (-x) : (x))
+
+#define SIZE 1024
+
 
 #endif
+
